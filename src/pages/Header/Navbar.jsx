bb19dc32b06452117c48f7b51eb3924205f62897
Ismael Cobo <midismael@gmail.com>
2022-05-25 14:34:34 +0200

    feat: Navbar component added

---
+++ src/pages/Header/Navbar.jsx
@@ -0,0 +1,14 @@
+
+import { Searcher } from '../../components/Searcher/Searcher'
+import { Container } from '../../styles/Container'
+
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <Container>
+        <Searcher />
+      </Container>
+    </nav>
+  )
+}
