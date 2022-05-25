66568326a807b874bdfefe5f3d71d6b60197fcc2
Ismael Cobo <midismael@gmail.com>
2022-05-25 14:34:24 +0200

    feat: Main component added

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,52 @@
+import styled from "@emotion/styled"
+import { SevenDays } from "../../components/SevenDays/SevenDays"
+import { Today } from "../../components/Today/Today"
+import { Tomorrow } from "../../components/Tomorrow/Tomorrow"
+
+export const Main = () => {
+  return (
+    <ContainerScroll>
+      <Section>
+        <Today />
+      </Section>
+
+      <Section>
+        <Tomorrow />
+      </Section>
+
+      <Section>
+        <SevenDays />
+      </Section>
+    </ContainerScroll>
+  )
+}
+
+const ContainerScroll = styled.div`
+  width: 100%;
+  height: 100%;
+  scroll-snap-type: x mandatory;
+  overflow-x: scroll;
+  display: flex;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`
+
+const Section = styled.section`
+  flex: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 5rem);
+  scroll-snap-align: start;
+  &:nth-of-type(1) {
+    background-color: blue;
+  }
+  &:nth-of-type(2) {
+    background-color: red;
+  }
+  &:nth-of-type(3) {
+    background-color: green;
+  }
+`
