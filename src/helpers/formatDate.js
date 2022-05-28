4d134f08758f8ea8512ca10441045f649794e36f
Ismael Cobo <midismael@gmail.com>
2022-05-28 17:23:18 +0200

    feat: 2 new functions

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,3 +1,6 @@
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 export const formatDate = (date) => {
   return new Date(date).toLocaleDateString('en', {
     month: 'long',
@@ -21,6 +24,18 @@ export const getHourFormatedWithTime = (date) => {
   return hoy.getHours()
 }
 
+export const getDayFormatedWithTime = (date) => {
+  const hoy = new Date(date)
+  return `${days[hoy.getDay()]}, ${hoy.getDate()} ${months[hoy.getMonth()]}`
+}
+
+export const isToday = (date) => {
+  let hoy = new Date()
+  hoy = `${hoy.getFullYear()}-${addZero(hoy.getMonth()+1)}-${hoy.getDate()}`
+  return hoy === date ? true : false
+}
+
 export const addZero = (number) => {
   return number < 10 ? `0${number}` : number
 } 
+
