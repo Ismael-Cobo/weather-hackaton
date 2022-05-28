7379ce9d0bceaa2970d0b78760842394ee1b0da7
Ismael Cobo <midismael@gmail.com>
2022-05-28 17:22:26 +0200

    feat: accordion component added

--- src/components/SevenDays/SevenDays.jsx
+++ src/components/SevenDays/SevenDays.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react"
+import styled from "@emotion/styled"
 
-export const SevenDays = () => {
+import { AccordionItem } from "../AccordionItem/AccordionItem"
+
+export const SevenDays = ({ weather }) => {
+
+  const [active, setActive] = useState(null)
+
+  const handleClick = (i) => {
+
+    if (i === active) {
+      return setActive(null)
+    }
+
+    setActive(i)
+  }
+
+
   return (
-    <p>SevenDays</p>
+    <AccordionWrapper>
+      {
+        weather.map((data, i) =>
+          <AccordionItem data={data} key={i} onClick={handleClick} i={i} active={active} />
+        )
+      }
+
+    </AccordionWrapper>
   )
 }
+
+const AccordionWrapper = styled.div`
+  width: 100%;
+`
+
