97bae5514dd00cb9f36b0143da1a0f4c48f7520d
Ismael Cobo <midismael@gmail.com>
2022-05-30 15:12:15 +0200

    feat: chart footer times

--- src/helpers/changeHours.js
+++ src/helpers/changeHours.js
@@ -12,3 +12,15 @@ export const separarFecha = (num) => {
   }
   return array
 }
+
+export const separarFechaText = (num) => {
+  let array = []
+
+  for (let i = 0; i < 5; i++) {
+    num = addZero(num)
+    if (num > 24) array.push(`${num - 25}:00`)
+    else array.push(`${num}:00`)
+    num++
+  }
+  return array
+}
