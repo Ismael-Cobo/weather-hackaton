4ea7ddecc60c5a89a848b4e1c8ee06e51f3ff96b
Ismael Cobo <midismael@gmail.com>
2022-06-01 17:31:47 +0200

    fix: styles fixed

--- src/appStyles.js
+++ src/appStyles.js
@@ -3,17 +3,14 @@ import styled from "@emotion/styled"
 
 export const AppWrapper = styled.main`
 display: grid;
-place-content: center;
+place-items: center;
 height: 100vh;
 `
 
 export const Card = styled.div`
-width: 320px;  
+width: 340px;  
 box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 border-radius: 10px;
-@media (min-width: 567px) {
-  width: 400px;
-}
 `
 
 export const HeaderWapper = styled.nav`
@@ -22,12 +19,9 @@ height: 100px;
 border-top-left-radius: 10px;
 border-top-right-radius: 10px;
 @media (min-width: 768px) {
-  width: 400px;
+  width: 340px;
 }
 `
 export const MainWrapper = styled.div`
-height: 500px;
-@media (min-width: 600px) {
-  height: 700px;
-}
+height: 600px;
 `
